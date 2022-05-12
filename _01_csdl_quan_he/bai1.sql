153448d9d4cbaf5c2a163a76a8098c812f872205
truongtran2212 <truongtran221298@gmail.com>
2022-05-12 08:15:54 +0700

    up

---
+++ _01_csdl_quan_he/bai1.sql
@@ -0,0 +1,12 @@
+create database student_management_two;
+use student_management_two;
+create table class(
+id int primary key auto_increment,
+`name` varchar(50)
+);
+create table teacher(
+id int primary key auto_increment,
+`name` varchar(50),
+age int,
+country varchar(50)
+);
---
+++ _01_csdl_quan_he/demo.sql
@@ -0,0 +1,6 @@
+create database student_management_two;
+use student_management_two;
+create table student(
+id int primary key auto_increment,
+`name` varchar(50)
+);
